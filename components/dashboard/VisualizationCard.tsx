3d88371e17637d30bc450f68d3c2616c4e7d3692
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-01-31 06:02:19 +0530

    initial setup

---
+++ components/dashboard/VisualizationCard.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+type Props = {}
+
+const VisualizationCard: FC<Props> = () => {
+  return (
+    <div>VisualizationCard</div>
+  )
+}
+
+export default VisualizationCard
---
+++ components/layout/SideBar.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react'
+
+type Props = {}
+
+const SideBar: FC<Props> = () => {
+  return (
+    <div>SideBar</div>
+  )
+}
+
+export default SideBar
