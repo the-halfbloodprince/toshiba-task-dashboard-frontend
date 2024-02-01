db729cf2797f411064cc9013133ab3f8cbd1cb02
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-02-01 09:06:54 +0530

    apex charts starting

--- components/dashboard/GrowthChart.tsx
+++ components/dashboard/GrowthChart.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import { MdArrowRightAlt as RightArrowIcon } from "react-icons/md";
+import { MdKeyboardArrowDown as ArrowDownIcon } from "react-icons/md";
 import styles from './GrowthChart.module.scss'
 
 type Props = {}
@@ -6,9 +8,11 @@ type Props = {}
 const GrowthChart: FC<Props> = ({}) => {
   return (
     <div className={styles.container}>
-      <h2>Growth</h2>
+      <div className={styles.header}>
+        <h2>Growth</h2>
+        <p>Yearly <ArrowDownIcon /></p>
+      </div>
       <div className={styles.chartArea}></div>
-      <div className={styles.link}>Link</div>
     </div>
   )
 }
--- components/dashboard/MeterVisualizationCard.tsx
+++ components/dashboard/MeterVisualizationCard.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react'
 import { MdArrowRightAlt as RightArrowIcon } from "react-icons/md";
+import Chart from 'react-apexcharts'
 import styles from './MeterVisualizationCard.module.scss'
+import { ApexOptions } from 'apexcharts';
 
 type Props = {
   text: string;
@@ -9,10 +11,67 @@ type Props = {
 }
 
 const MeterVisualizationCard: FC<Props> = ({ text, percentage, linkText }) => {
+
+  const chartSeries = [percentage]
+
+  const chartOptions: ApexOptions = {
+    chart: {
+      type: 'radialBar',
+      offsetY: 0,
+      sparkline: {
+        enabled: true
+      }
+    },
+    stroke: {
+      lineCap: 'round'
+    },
+    plotOptions: {
+      radialBar: {
+        startAngle: -90,
+        endAngle: 90,
+        track: {
+          background: "#FFF7E8",
+          strokeWidth: '100%',
+          margin: 10, // margin is in pixels
+          dropShadow: {
+            enabled: false,
+            top: 2,
+            left: 0,
+            color: '#999',
+            opacity: 1,
+            blur: 2
+          }
+        },
+        dataLabels: {
+          name: {
+            show: false
+          },
+          value: {
+            offsetY: -2,
+            fontSize: '22px',
+            fontWeight: 600,
+            fontFamily: 'sans-serif'
+          }
+        }
+      }
+    },
+    grid: {
+      padding: {
+        // top: -10
+      }
+    },
+    fill: {
+      colors: ['#FFCD71']
+    },
+    labels: [text],
+  };
+
   return (
     <div className={styles.container}>
       <h2>Quarter Goal</h2>
-      <div className={styles.chartArea}></div>
+      <div className={styles.chartArea}>
+        <Chart options={chartOptions} series={chartSeries} type="radialBar" />
+      </div>
       {/* link */}
       <div className={styles.link}>
         <div className={styles.linkText}>All goals</div>
