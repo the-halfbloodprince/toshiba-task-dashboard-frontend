670c541510982996e986501b4fbc65879608ec60
Manish Kumar Das <ammanishkumardas@gmail.com>
2024-02-01 10:24:24 +0530

    growth chart

--- components/dashboard/GrowthChart.tsx
+++ components/dashboard/GrowthChart.tsx
@@ -1,18 +1,116 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import { MdArrowRightAlt as RightArrowIcon } from "react-icons/md";
 import { MdKeyboardArrowDown as ArrowDownIcon } from "react-icons/md";
+import ReactApexChart from 'react-apexcharts'
 import styles from './GrowthChart.module.scss'
+import { ApexOptions } from 'apexcharts';
 
 type Props = {}
 
 const GrowthChart: FC<Props> = ({}) => {
+
+  const [dataSeries, setDataSeries] = useState<number[][]>([])
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_HOST}${process.env.NEXT_PUBLIC_DATA_ENDPOINT}`)
+      const data = await res.json()
+      const newSeries = []
+      for(let row of data) {
+        console.log(row)
+        newSeries.push([Date.parse(row['Timestamp']), Number.parseFloat(row['Profit Percentage'])])
+      }
+      setDataSeries(newSeries)
+    }
+
+    fetchData()
+  }, [])
+
+  const options: ApexOptions = {
+      yaxis: {
+        labels: {
+          formatter: (value) => `${Math.round(value)}`
+        }
+      },
+      colors: ['#25CD25'],
+      stroke: {
+        curve: 'straight',
+        width: 2,
+        colors: ['#25CD25']
+      },
+      chart: {
+        id: 'area-datetime',
+        type: 'area',
+        // height: 250,
+        height: 200,
+        // width: 400,
+        zoom: {
+          autoScaleYaxis: true
+        }
+      },
+      annotations: {
+        yaxis: [{
+          y: 30,
+          borderColor: '#999',
+          label: {
+            text: 'Support',
+            style: {
+              color: "#fff",
+              background: '#00E396'
+            }
+          }
+        }],
+        xaxis: [{
+          x: new Date('14 Nov 2012').getTime(),
+          borderColor: '#999',
+          // yAxisIndex: 0,
+          label: {
+            text: '',
+            style: {
+              color: "#fff",
+              background: '#775DD0'
+            }
+          }
+        }]
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: 'datetime',
+        // min: new Date('01 Mar 2012').getTime(),
+        // tickAmount: 6,
+      },
+      tooltip: {
+        x: {
+          format: 'dd MMM yyyy'
+        }
+      },
+      fill: {
+        // type: 'gradient',
+        // gradient: {
+        //   shadeIntensity: 1,
+        //   opacityFrom: 0.7,
+        //   opacityTo: 0.9,
+        //   stops: [0, 100]
+        // }
+      },
+    }
+
+  // const selection = 'one_year'
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <h2>Growth</h2>
         <p>Yearly <ArrowDownIcon /></p>
       </div>
-      <div className={styles.chartArea}></div>
+      <div className={styles.chartArea}>
+        <ReactApexChart options={options} series={[{ data: dataSeries }]} />
+      </div>
     </div>
   )
 }
--- data/customers.ts
+++ data/customers.ts
@@ -31,4 +31,16 @@ export const customers: Customer[] = [
     organization: 'Cuchamucha',
     imageSrc: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
   },
+  {
+    id: 6,
+    name: 'Javier Penna',
+    organization: 'DEA',
+    imageSrc: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
+  {
+    id: 7,
+    name: 'Henry Stanford',
+    organization: 'Apple',
+    imageSrc: 'https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+  },
 ] 
